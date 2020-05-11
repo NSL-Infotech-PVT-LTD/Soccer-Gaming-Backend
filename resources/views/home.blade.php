525f8670c389135949b6f89a28fcff4281d7df9d
shivam tyagi <root@netscapelabs-HP-Laptop-14q-cs0xxx>
2020-05-11 18:14:13 +0530

    merged branches

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,43 @@
 
 @section('content')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+<style>
+    .main-overview {
+        height: 169px;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(265px, 1fr)); /* Where the magic happens */
+        grid-auto-rows: 94px;
+        grid-gap: 20px;
+        margin: 20px;
+    }
+
+    .overviewcard {
+        height: 169px;
+        color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px;
+        background-color: #382e72;
+        text-decoration: none;
+        border-radius: 10px;
+    }
+    
+    .overviewcard__icon {
+        font-size: 30px;
+        text-decoration: none;
+    }
+    .overviewcard__info {
+        font-size: 27px;
+        text-decoration: none;
+    }
+    .overviewcard:hover {
+        background-color: #2b303a;
+    }
+</style>
 <div class="container">
 
-    <div class="card">
-        <div class="card-header"><h3 class="card-text animated  rubberBand delay-1s"><b>Dashboard</b></h3></div>
-   
-    </div>  
+      
     <div class="col-md-12">
         <div class="row">
             <div class="col-md-4">
@@ -28,9 +59,9 @@
                 <a href="{{url('admin/ads')}}" class="links" style="text-decoration: none;">
                     <div class="main-overview">
                         <div class="overviewcard">
-                            <div class="overviewcard__icon">Total Ads&nbsp;&nbsp;<i class="fa fa-picture-o"></i></div>
+                            <div class="overviewcard__icon">Total Tournaments&nbsp;&nbsp;<i class="fa fa-picture-o"></i></div>
                             <div class="overviewcard__info"><?php
-                                echo count($users = App\Ad::get());
+                                echo count($users = App\Tournament::get());
                                 ?></div>
                         </div>
                     </div>
@@ -40,9 +71,9 @@
                 <a href="{{url('admin/features')}}" class="links" style="text-decoration: none;">
                     <div class="main-overview">
                         <div class="overviewcard">
-                            <div class="overviewcard__icon">Total Free Features&nbsp;&nbsp;<i class="fa fa-unlock"></i></div>
+                            <div class="overviewcard__icon">Total Teams&nbsp;&nbsp;<i class="fa fa-unlock"></i></div>
                             <div class="overviewcard__info"><?php
-                                echo count($users = App\Feature::get());
+                                //echo //count($users = App\Feature::get());
                                 ?></div>
                         </div>
                     </div>
@@ -50,45 +81,7 @@
             </div>
 
         </div>
-        <div class="row">
-            <div class="col-md-4">
-                <a href="{{url('admin/users/subscribed')}}" class="links" style="text-decoration: none;">
-                    <div class="main-overview">
-                        <div class="overviewcard">
-                            <div class="overviewcard__icon">Subscribed Users&nbsp;&nbsp;<i class="fa fa-users"></i></div>
-                            <div class="overviewcard__info"><?php
-                                echo count($users = App\User::where('id', '!=', Auth::id())->where('payment_status', '=', '2')->get());
-                                ?></div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-4">
-                <a href="{{url('admin/users/unsubscribed')}}" class="links" style="text-decoration: none;">
-                    <div class="main-overview">
-                        <div class="overviewcard">
-                            <div class="overviewcard__icon">UnSubscribed Users&nbsp;&nbsp;<i class="fa fa-users"></i></div>
-                            <div class="overviewcard__info"><?php
-                                echo count($users = App\User::where('id', '!=', Auth::id())->where('payment_status', '=', '1')->get());
-                                ?></div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="{{url('admin/users/freetrial')}}" class="links" style="text-decoration: none;">
-                    <div class="main-overview">
-                        <div class="overviewcard">
-                            <div class="overviewcard__icon">On Free Trial&nbsp;&nbsp;<i class="fa fa-users"></i></div>
-                            <div class="overviewcard__info"><?php
-                                echo count($users = App\User::where('id', '!=', Auth::id())->where('payment_status', '=', '0')->get());
-                                ?></div>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
+        
     </div>
 </div>
 @endsection
