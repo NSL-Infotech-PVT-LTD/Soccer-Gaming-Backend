5bfcde230ddd9ba980db9f97dd768ea93a6fa658
shivam tyagi <root@netscapelabs-HP-Laptop-14q-cs0xxx>
2020-06-17 19:57:04 +0530

    header removed in accept friend request API

--- app/Http/Controllers/API/TournamentsController.php
+++ app/Http/Controllers/API/TournamentsController.php
@@ -589,7 +589,7 @@ class TournamentsController extends ApiController {
             $frienddata = \App\UserFriend::where([['user_id', $request->friend_id], ['friend_id', \Auth::id()]])->update(['status' => $request->status]);
 
         if($request->status == 'accepted'):
-            parent::pushNotifications(['title' => '', 'body' => 'Your Friend Request has been accepted', 'data' => ['target_id' => \Auth::id(), 'target_model' => 'UserFriend', 'data_type' => 'FriendRequest']], $request->friend_id, TRUE);
+            parent::pushNotifications(['body' => 'Your Friend Request has been accepted', 'data' => ['target_id' => \Auth::id(), 'target_model' => 'UserFriend', 'data_type' => 'FriendRequest']], $request->friend_id, TRUE);
         endif;
             
 
