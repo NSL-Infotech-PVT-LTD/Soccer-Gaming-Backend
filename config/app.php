f481a544ceb71fd93070014fa3399fd2527f4676
shivam tyagi <root@netscapelabs-HP-Laptop-14q-cs0xxx>
2020-10-08 11:21:39 +0530

    utc time

--- config/app.php
+++ config/app.php
@@ -58,7 +58,7 @@ return [
       | ahead and set this to a sensible default for you out of the box.
       |
      */
-    'timezone' => 'Asia/Kolkata',
+    'timezone' => 'UTC',
     /*
       |--------------------------------------------------------------------------
       | Application Locale Configuration
