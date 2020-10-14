2e90de2b72a3ee24b4be1e13450ea13780aaea2e
shivam tyagi <root@netscapelabs-HP-Laptop-14q-cs0xxx>
2020-10-14 12:04:16 +0530

    delete players and edit image

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -2,7 +2,7 @@
     {!! Form::label('username', 'Username: ', ['class' => 'control-label']) !!}
     {!! Form::text('username', null, ['class' => 'form-control', 'required' => 'required']) !!}
     {!! $errors->first('username', '<p class="help-block">:message</p>') !!}
-</div>-->
+</div>--> 
 <div class="form-group{{ $errors->has('first_name') ? ' has-error' : ''}}">
     {!! Form::label('first_name', 'First Name: ', ['class' => 'control-label']) !!}
     {!! Form::text('first_name', null, ['class' => 'form-control', 'required' => 'required']) !!}
